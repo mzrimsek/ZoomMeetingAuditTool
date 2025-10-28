8276ee10983f0cfbc655cfffad5a7323ac0e2fc1
= <=>
2025-10-28 19:21:55 -0400

    more tutorial testing

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require("electron/main");
+const { app, BrowserWindow, ipcMain } = require("electron/main");
+
 const path = require("node:path");
 
 const createWindow = () => {
@@ -9,22 +10,9 @@ const createWindow = () => {
       preload: path.join(__dirname, "preload.js"),
     },
   });
-
   win.loadFile("index.html");
 };
-
 app.whenReady().then(() => {
+  ipcMain.handle("ping", () => "pong");
   createWindow();
-
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
 });
